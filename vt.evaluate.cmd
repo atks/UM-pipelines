92ec45cea1b90e4d9b6066b7b84bf36340d2a4bb
Adrian Tan <atks@umich.edu>
2014-08-11 17:25:02 -0400

    updated vt.evaluate.cmd example

--- vt.evaluate.cmd
+++ vt.evaluate.cmd
@@ -1,15 +1,12 @@
-/net/sardinia/atks/sardinia/20140121_variant_calling/generate_gatk_calling_makefile.pl \
--w /net/sardinia/atks/sardinia/20140121_variant_calling \
--o run_gatk_ug_indels \
--b /net/sardinia/atks/sardinia/20140121_variant_calling/codes/vt \
--t /net/sardinia/atks/sardinia/20140121_variant_calling/codes/cluster \
--m sardinia.low_coverage.gatk.ug.indels.calling.mk \
+/net/fantasia/home/atks/dev/vt/pipeline/generate_variant_evaluation_pipeline_makefile.pl \
+-g /net/fantasia/home/atks/bipolar/20140807_low_coverage_evaluation/bipolar.low_coverage.genotype.vcf.fileslist.txt \
+-o /net/fantasia/home/atks/bipolar/20140807_low_coverage_evaluation \
+-b /net/fantasia/home/atks/dev/vt \
+-t /net/fantasia/home/atks/programs/cluster \
+-m bipolar.low_coverage.gatk.ug.indels.evaluation.mk \
 -c mini+ \
--d 0 \
--s 2210samples.sardinia.low_coverage.sa \
--u /net/sardinia/atks/sardinia/20140121_variant_calling/run_gatk_ug_indels/bam.list \
--l seq.length.txt \
--i 1000000 \
--r /net/sardinia/atks/sardinia/20140121_variant_calling/hs37d5.fa \
--j 8g \
+-d 25 \
+#-s sample.sa \
+#-p pedigree.ped \
+-r /net/bipolar/atks/ref/hs37d5.fa \
 -v INDEL
